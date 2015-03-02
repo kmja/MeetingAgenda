470d6d7e8fa9c27af32b3984fd31218c0657419a
fredrik-eliasson <felia@kth.se>
2015-03-02 16:11:06 +0100

    Added new activity for day

--- app/src/main/java/com/example/karl/meetingagenda/android/DayActivity.java
+++ app/src/main/java/com/example/karl/meetingagenda/android/DayActivity.java
@@ -12,7 +12,7 @@ public class DayActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.day_layout);
+        setContentView(R.layout.dayLayout);
     }
 
 
