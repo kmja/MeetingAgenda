328eeeb9dae74613a20aedcd4f6f2e01379a5622
kmja <karl.mj.andersson@gmail.com>
2015-03-02 16:08:28 +0100

    layout dayscreen

--- app/src/main/java/com/example/karl/meetingagenda/android/ActivityActivity.java
+++ app/src/main/java/com/example/karl/meetingagenda/android/ActivityActivity.java
@@ -13,7 +13,7 @@ public class ActivityActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_);
+        setContentView(R.layout.activityLayout);
     }
 
 
