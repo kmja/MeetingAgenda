0c29f93f601f4b496c0bea4102722b1292afc979
kmja <karl.mj.andersson@gmail.com>
2015-03-20 12:30:35 +0100

    arrows, drag n drop touch fields

--- app/src/main/java/com/example/karl/meetingagenda/android/DayActivity.java
+++ app/src/main/java/com/example/karl/meetingagenda/android/DayActivity.java
@@ -88,11 +88,7 @@ public class DayActivity extends Activity {
         view.view.setOnTouchListener(touchListener);
 
     }
-    //skulle lägga till intents för pilar höger/vänster. Gjorde först en ny onclicklistener = (v) -> etc som nedan, men
-        // det knasade sig, så jag lade in koden i samma clickhandler istället. Nu är det något som har fuckat med den gamla också.
-        // Vid en punkt klickade jag på (v) -> nedan (som då var grönmarkerad) och det expanderade till new OnClickListener blabla.
-        // Tog bort det och skrev dit det som står nu, men det är inte längre grönmarkerat. Nu är det rödmarkerat på clickHandler
-        // och touchListener ovan, och på Override står det "annotations are not allowed here".
+
     View.OnClickListener clickHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
